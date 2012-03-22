8de8b093b0c29fc74ea610ae6c365232a42dfe10
JE Bailey <jason.bailey@24601.org>
2012-03-21 21:10:33 -0400

    adding math abilities addon

---
+++ posl-lang-math/src/posl/lang/Relational.java
@@ -0,0 +1,25 @@
+package posl.lang;
+
+import posl.engine.annotation.Command;
+import posl.lang.math.NumberMath;
+
+
+public class Relational {
+
+	@Command("<")
+	public static Object lt(Number left, Number right) throws Exception {
+		return NumberMath.compareTo(left,right) < 0;
+
+	}
+	
+	@Command(">")
+	public static Object gt(Number left, Number right) throws Exception {
+		return NumberMath.compareTo(left,right) > 0;
+	}
+	
+	@Command("=")
+	public static Object equal(Number left, Number right) throws Exception {
+		return NumberMath.compareTo(left, right) == 0;
+	}
+	
+}
