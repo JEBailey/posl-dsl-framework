e9aeac14ac76fef5cd5b15ef499b8a9b111bc48a
jebailey <jason.bailey@24601.org>
2012-04-24 12:12:26 -0400

    updated comments

--- posl-engine/src/posl/editorkit/util/BoxHighlightPainter.java
+++ posl-engine/src/posl/editorkit/util/BoxHighlightPainter.java
@@ -2,6 +2,7 @@ package posl.editorkit.util;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.Shape;
 
@@ -11,6 +12,13 @@ import javax.swing.text.Position;
 import javax.swing.text.View;
 import javax.swing.text.DefaultHighlighter.DefaultHighlightPainter;
 
+/**
+ * Creates a square box around the matching token
+ * to provide a visual cue to the opening and closing 
+ * brace
+ * 
+ * @author JE Bailey
+ */
 public class BoxHighlightPainter extends DefaultHighlightPainter {
 
 	public BoxHighlightPainter(Color color) {
@@ -22,7 +30,7 @@ public class BoxHighlightPainter extends DefaultHighlightPainter {
 			int offs1, Shape bounds, JTextComponent c,
 			View view) {
 		Rectangle alloc = null;
-		g.setColor(Color.gray);
+		g.setColor(getColor());
 		try {
 			alloc = (Rectangle) view.modelToView(offs0,
 					Position.Bias.Forward, offs1,
@@ -30,9 +38,7 @@ public class BoxHighlightPainter extends DefaultHighlightPainter {
 		} catch (BadLocationException e) {
 			return null;
 		}
-		g.drawRect(alloc.x, alloc.y, alloc.width - 1,
-				alloc.height - 1);
-
+		g.drawRect(alloc.x, alloc.y, alloc.width - 1,alloc.height - 1);
 		return alloc;
 	}
 
