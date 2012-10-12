1a8e4b6f6201a314824af65bc95ed0d099731cfa
jebailey <jason.bailey@24601.org>
2012-10-11 22:26:28 -0400

    corrected position values for EOL

--- posl-engine/src/posl/engine/lexeme/Eol.java
+++ posl-engine/src/posl/engine/lexeme/Eol.java
@@ -14,21 +14,26 @@ public class Eol extends ALexeme {
 
 	@Override
 	public boolean consume(List<IToken> tokens, PoslStream ps) {
+		ps.mark();
 		if (ps.val() == -1){
-			tokens.add(new Inner());
+			tokens.add(new Inner(ps));
 			return false;
 		}
 		while (ps.val() == '\n'){
 			ps.pop();
-			return tokens.add(new Inner());
+			return tokens.add(new Inner(ps));
 		}
 		return false;
 	}
 	
 	private class Inner extends IToken {
 		
-		public Inner(){
+		
+		
+		public Inner(PoslStream ps){
 			this.value = "\n";
+			this.startPos = ps.getMark();
+			this.endPos = ps.getMark();
 		}
 		
 		public IStatement consume(IStatement statement, Stack<IStatement> statements,
