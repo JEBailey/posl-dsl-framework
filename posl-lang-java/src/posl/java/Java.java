24c6212bc3b8424a853ac368048d8e1cc2733565
jason bailey <jason.bailey@24601.org>
2013-08-06 15:25:43 -0400

    update to statement interface

--- posl-lang-java/src/posl/java/Java.java
+++ posl-lang-java/src/posl/java/Java.java
@@ -74,7 +74,7 @@ public class Java implements Executable {
 	public Object execute(Scope scope, SingleStatement tokens) throws PoslException {
 		if(tokens.size() == 1) return this;
 		String methodName = scope.get(Atom.class,tokens.get(1)).toString();
-		SingleStatement args = new SingleStatement();
+		SingleStatement args = new SingleStatement(tokens.startPos());
 		for(int i = 2; i < tokens.size(); i++){
 			args.add(tokens.get(i));
 		}
