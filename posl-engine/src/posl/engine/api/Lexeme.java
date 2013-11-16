46dd83df0029a2250aa8be36ada88f8324e4162c
jebailey <jason.bailey@24601.org>
2013-11-16 08:23:20 -0500

    added documentation

--- posl-engine/src/posl/engine/api/Lexeme.java
+++ posl-engine/src/posl/engine/api/Lexeme.java
@@ -5,7 +5,14 @@ import java.util.List;
 import posl.engine.core.Stream;
 
 public interface Lexeme {
-	
+	/**
+	 * Converts the input stream into one or more
+	 * tokens that are added to the token list
+	 * 
+	 * @param tokens
+	 * @param ps
+	 * @return true if text from the stream was consumed
+	 */
 	boolean consume(List<Token> tokens, Stream ps);
 	
 }
