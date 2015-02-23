4238ef9b9319b19b9062b58908b69aeae736aa52
jebailey <jason.bailey@24601.org>
2015-02-22 22:33:13 -0500

    Fixed an error in namespace where it wouldn't have recognized values across deep heirarchies of unrelated scopes

--- posl-lang-core/src/posl/lang/executable/NamespaceExec.java
+++ posl-lang-core/src/posl/lang/executable/NamespaceExec.java
@@ -8,7 +8,7 @@ import posl.engine.type.MultiLineStatement;
 import posl.engine.type.Statement;
 
 /**
- * This defines the creation of a Name
+ * Creates a Scope and populates with a set of statements. 
  * 
  * 
  * @author jebailey
@@ -17,42 +17,63 @@ import posl.engine.type.Statement;
 public class NamespaceExec implements Executable {
 
 	/**
-	 * Parent scope.
+	 * Namespace scope.
 	 */
 	private Scope runtimeScope;
-	
 
-	public NamespaceExec(Scope scope,MultiLineStatement statements) throws PoslException {
+	/**
+	 * Create a new Scope and populate
+	 * 
+	 * @param scope parent scope
+	 * @param statements to be evaluated
+	 * @throws PoslException
+	 */
+	public NamespaceExec(Scope scope, MultiLineStatement statements)
+			throws PoslException {
 		runtimeScope = scope.createChildScope();
-		Interpreter.process(runtimeScope,statements);
+		Interpreter.process(runtimeScope, statements);
 	}
-	
-	public Object processStatements(MultiLineStatement statements) throws PoslException{
-		return Interpreter.process(runtimeScope,statements);
+
+	/**
+	 * 
+	 * 
+	 * 
+	 * @param statements to be evaluated
+	 * @return
+	 * @throws PoslException
+	 */
+	public Object processStatements(MultiLineStatement statements)
+			throws PoslException {
+		return Interpreter.process(runtimeScope, statements);
 	}
-	
-	public Object processStatement(Statement statement) throws PoslException{
-		return Interpreter.process(runtimeScope,statement);
+
+	public Object processStatement(Statement statement) throws PoslException {
+		return Interpreter.process(runtimeScope, statement);
 	}
-	
-	public void put(String key,Object value){
+
+	public void put(String key, Object value) {
 		this.runtimeScope.put(key, value);
 	}
 
 	@Override
-	public Object execute(Scope callingScope, Statement tokens) throws PoslException {
+	public Object execute(Scope callingScope, Statement tokens)
+			throws PoslException {
 		try {
-			Statement subList = tokens.subList(1,tokens.size());
-			return Interpreter.process(runtimeScope, subList);
+			Object token  = runtimeScope.getValue(tokens.get(1));
+			if (token instanceof Executable) {
+				token = ((Executable) token).execute(callingScope, tokens.subList(1, tokens.size()));
+			}
+			return token;
 		} catch (PoslException e) {
-			throw e.push(tokens.startPos(), "in namespace "+tokens.get(0).toString());
+			throw e.push(tokens.startPos(), "in namespace "
+					+ tokens.get(0).toString());
 		}
 	}
 
 	public boolean containsKey(String key) {
 		return runtimeScope.containsKey(key);
 	}
-	
+
 	@Override
 	public String toString() {
 		return "NAMESPACE :: " + super.toString();
