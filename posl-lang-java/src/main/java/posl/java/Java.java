05d6b26f1c50cda66ee4e3a99683e35fbe6f6a34
jebailey <jason.bailey@24601.org>
2015-07-25 12:54:30 -0400

    minor update. I will need to fully flesh out the interactions with java at some point since it tends to be a requested feature.

--- posl-lang-java/src/main/java/posl/java/Java.java
+++ posl-lang-java/src/main/java/posl/java/Java.java
@@ -74,7 +74,7 @@ public class Java implements Executable {
 	public Object execute(Scope scope, Statement tokens) throws PoslException {
 		if(tokens.size() == 1) return this;
 		String methodName = scope.get(Atom.class,tokens.get(1)).toString();
-		Statement args = new Statement(tokens.startPos());
+		Statement args = new Statement(tokens.startPos(),0);
 		for(int i = 2; i < tokens.size(); i++){
 			args.add(tokens.get(i));
 		}
